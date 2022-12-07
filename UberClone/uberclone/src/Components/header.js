159331daa40cd610dc5f76482477f735967c6d8d
Mohammedirbaaz <irbaaz308@gmail.com>
2022-12-07 21:14:02 +0530

    started working on uberclone

---
+++ UberClone/uberclone/src/Components/header.js
@@ -0,0 +1,11 @@
+import React,{useState,useEffect} from 'react';
+import './css/header.css';
+
+function header(){
+    return(
+        <div className='header'>
+            
+        </div>)
+}
+
+export default header;
---
+++ UberClone/uberclone/src/Components/home.js
@@ -0,0 +1,11 @@
+import React,{useState,useEffect} from 'react';
+import Header from './header';
+import Footer from './footer';
+function home(){
+    return(
+        <div>
+            <Header/>
+        </div>)
+}
+
+export default home;
